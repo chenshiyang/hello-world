2320bf59755c21a7c05cd3045b97a39e6daa0916
csy <411030065@qq.com>
2016-12-30 16:31:31 +0800

    Second time commit, test Chinese character.

---
+++ src/com/csy/demo/Hello.java
@@ -0,0 +1,10 @@
+package com.csy.demo;
+
+/**
+ * Created by csy on 2016/12/30.
+ */
+public class Hello {
+    public static void main(String[] args) {
+        System.out.println("最缤纷的花园游乐过。");
+    }
+}
